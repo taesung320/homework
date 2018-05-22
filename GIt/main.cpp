c3278eb442e62e72b3d312e48c6f406fcc739c97
taesung320 <taesung320@gmail.com>
2018-05-23 03:16:56 +0900

    Initial Commit

---
+++ GIt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GIt
+//
+//  Created by saesol on 2018. 5. 23..
+//  Copyright © 2018년 scit3. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
