81176893ba94b7639aa372de6065976fa3094079
taesung320 <taesung320@gmail.com>
2018-05-23 04:39:14 +0900

    My first attempt

--- GIt/main.cpp
+++ GIt/main.cpp
@@ -8,8 +8,32 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{bool arr[3][4];
+    for(int a=0;a<3;a++)
+    {
+        for(int b=0;b<4;b++)
+        {
+            cin>>arr[a][b];
+            
+        }
+        cout<<endl;
+    }
+    
+    for(int a=0;a<3;a++)
+    {
+        int n=0;
+        for(int b=0;b<4;b++)
+        {
+            if(!arr[a][b]) n++;
+        }
+        char c=64+n;
+        if(n) {}
+        else {c=69;}
+        cout<<c<<endl;
+
+    }
+    
 }
